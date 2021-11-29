a4f53191079f3ef7a5cbf188c1cbfd60c7245338
Laura Cañizares Herrera <alu0101129945@ull.edu.es>
2021-11-29 13:31:37 +0000

    feat: add user item card to store :gear:

--- torch-front/src/store/index.js
+++ torch-front/src/store/index.js
@@ -19,6 +19,10 @@ export default new Vuex.Store({
       endedRoutinesList: [],
       createdRoutinesList: [], 
     },
+    userItemCard: {
+      image: '',
+      active: false,
+    }
   },
   mutations: {
   },
