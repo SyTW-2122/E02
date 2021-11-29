7a4a63b5673c906e55e6dc2bc9419e306cb98601
Eric DS <eric.d.s.info@gmail.com>
2021-11-29 13:18:47 +0000

    feat: add user state object :gear:

--- torch-front/src/store/index.js
+++ torch-front/src/store/index.js
@@ -5,6 +5,20 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: {
+      userName: '',
+      subtitle: '',
+      name: '',
+      profileImage: '',
+      password: '',
+      email: '',
+      sportStatsList: [],
+      socialtStatsList: [],
+      followers: 0,
+      following: 0,
+      endedRoutinesList: [],
+      createdRoutinesList: [], 
+    },
   },
   mutations: {
   },
