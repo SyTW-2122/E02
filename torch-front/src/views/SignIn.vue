0d419349b5e9fc26252a37db26bd604be8975bf9
Alejandro Martín de León <alu0101015941@ull.edu.es>
2021-12-09 22:17:48 +0000

    feat: updating sign in layout :gear:

--- torch-front/src/views/SignIn.vue
+++ torch-front/src/views/SignIn.vue
@@ -1,5 +1,59 @@
 <template>
   <div class="sign-in">
-    <h1>This is the sign in page</h1>
+    <div class="welcome">
+      <img src="../assets/torch-logo.png" alt="">
+      <h1>Welcome</h1>
+    </div>
+    <div class="passport-buttons">
+      <button class="login-google">Login with Google</button>
+      <button class="login-facebook">Login with Facebook</button>
+    </div>
+    <div class="separator">
+      <div class="line"></div>
+      <p>or</p>
+      <div class="line"></div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 150px;
+}
+
+h1 {
+  text-align: center;
+}
+
+button {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.separator {
+  display: flex;
+  align-items: center;
+  margin-left: 50px;
+  margin-right: 50px;
+}
+
+.separator p {
+  padding: 0 10px;
+  opacity: 0.2;
+  color: #000;
+}
+
+.separator .line {
+  flex: 1;
+  height: 1px;
+  background-color: #000;
+  opacity: 0.1
+}
+
+</style>
