6fe290dafc0ba17bd1df7f594dbd59a354f442ad
Eric DS <eric.d.s.info@gmail.com>
2021-12-10 20:27:30 +0000

    feat: add session handling in login form :rocket:

--- torch-front/src/services/auth-service.js
+++ torch-front/src/services/auth-service.js
@@ -3,23 +3,19 @@ import axios from 'axios';
 const API_URL = 'http://localhost:5000/api/auth/';
 
 class AuthService {
-  static login(user) {
-    return axios
-      .post(`${API_URL}login`, {
-        username: user.username,
-        password: user.password,
-      })
-      .then((response) => {
-        if (response.data.accessToken) {
-          localStorage.setItem('user', JSON.stringify(response.data));
-        }
-        return response.data;
-      });
-  }
+  login = (user) => axios
+    .post(`${API_URL}login`, {
+      username: user.username,
+      password: user.password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem('user', JSON.stringify(response.data));
+      }
+      return response.data;
+    });
 
-  static logout() {
-    localStorage.removeItem('user');
-  }
+  logout = () => localStorage.removeItem('user');
 }
 
 /* logout() and register() */
