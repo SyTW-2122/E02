65cdd15102b96c4f3315fe8e2c11cd6e7e04b07a
Laura Cañizares Herrera <alu0101129945@ull.edu.es>
2021-12-15 18:11:54 +0000

    feat: Add sign up responsive layout :gear:

--- torch-front/src/views/SignUp.vue
+++ torch-front/src/views/SignUp.vue
@@ -1,122 +1,99 @@
 <template>
-<b-container class="bv-example-row">
-  <b-row>
-    <b-col></b-col>
-    <b-col cols="8">
-    <br />
-      <a href="#">
-        <center><img
-          src="../assets/torch-logo_Mesa de trabajo 1 copia.png"
-          alt=""
-          class="img-fluid"></center>
-      </a>
-    </b-col>
-    <b-col></b-col>
-  </b-row>
-  <br />
-   <b-row>
-    <b-col></b-col>
-    <b-col>
-      <center><h1> Welcome </h1></center>
-    </b-col>
-    <b-col></b-col>
-  </b-row>
-  <br />
-  <b-row class="text-center">
-    <b-col></b-col>
-    <b-col cols="8">
-      <b-button pill variant="outline-secondary" style="width: 100%;">
-      <img src="../assets/google.png" class="emo">&nbsp;&nbsp;Sign up with Google</b-button>
-    </b-col>
-    <b-col></b-col>
-  </b-row>
-  <br />
-   <b-row class="text-center">
-    <b-col></b-col>
-    <b-col cols="8">
-      <b-button pill variant="primary" style="width: 100%;" class="button-face">
-      <img src="../assets/facebook-brands.svg" class="emo-face">&nbsp;Sign up with Facebook
-      </b-button>
-    </b-col>
-    <b-col></b-col>
-  </b-row>
-  <br />
-  <b-row>
-    <b-col>
-      <div class="linea"></div>
-    </b-col>
-    <b-col>
-      <center><p>Or</p></center>
-    </b-col>
-    <b-col>
-      <div class="linea"></div>
-    </b-col>
-  </b-row>
-  <b-row>
-    <b-col></b-col>
-    <b-col cols="8">
-      <form name="form" @submit.prevent="handleRegister">
-        <div v-if="!successTrue()">
-          <div class="form-group">
-            <label for="username">Username</label>
-            <input
-              v-model="user.username"
-              v-validate="'required|min:3|max:20'"
-              type="text"
-              class="form-control"
-              name="username"
-              placeholder="Enter username"
-            />
+  <div class="container">
+    <div class="row">
+      <div class="logo col-12">
+        <img src="../assets/images/torch-logo.png" alt="Logo" class="img-fluid">
+      </div>
+      <div class="col-12 text-center mt-2">
+        <h1 class="display-4 font-weight-bold">Welcome</h1>
+      </div>
+      <div class="login-google col-12 col-md-6 text-center mt-2">
+        <a class="btn btn-primary btn-lg mb-1">
+          <img src="../assets/images/google.png" alt="Google" class="img-fluid h-100">
+          Login with Google
+        </a>
+      </div>
+      <div class="login-facebook col-12 col-md-6 text-center mt-2">
+        <a class="btn btn-primary btn-lg mb-3">
+          <img src="../assets/images/facebook.svg" alt="Logo" class="img-fluid h-100">
+          Login with Facebook
+        </a>
+      </div>
+      <div class="line col-5 mt-2"></div>
+      <div class="or col-2 text-center">Or</div>
+      <div class="line col-5 mt-2"></div>
+      <div class="login-form col-12 text-center mt-2">
+        <b-form @submit.prevent="handleLogin">
+          <b-form-group
+            id="input-group-email"
+            label=""
+            label-for="input-email"
+            class="input-group-email text-center"
+          >
+            <b-form-input
+                id="input-email"
+                type="text"
+                v-model="user.email"
+                v-validate="'required'"
+                placeholder="Email Address"
+                class="input-email border-bottom"
+            ></b-form-input>
             <div
-            v-if="submitTrue() && errors.has('username')"
-            class="bg-danger"
-            >{{errors.first('username')}}</div>
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input
-              v-model="user.password"
-              v-validate="'required|min:6|max:40'"
+              v-if="errors.has('email')"
+              class="alert alert-danger"
+              role="alert"
+            >Email address is required!</div>
+          </b-form-group>
+          <b-form-group
+            id="input-group-password"
+            label=""
+            label-for="input-password"
+            class="input-group-password mt-2"
+          >
+            <b-form-input
+                id="input-password"
+                type="password"
+                v-model="user.password"
+                v-validate="'required'"
+                placeholder="Password"
+                class="input-password border-bottom"
+            ></b-form-input>
+            <div
+              v-if="errors.has('password')"
+              class="alert alert-danger"
+              role="alert"
+            >Password is required!</div>
+          </b-form-group>
+          <b-form-group
+            id="input-group-repeat-password"
+            label=""
+            label-for="input-password"
+            class="input-group-repeat-password mt-2"
+            >
+            <b-form-input
+              id="input-password"
               type="password"
-              class="form-control"
-              name="password"
-            />
+              v-model="user.password"
+              v-validate="'required'"
+              placeholder="Repeat password"
+              class="input-password border-bottom"
+            ></b-form-input>
             <div
-              v-if="submitTrue() && errors.has('password')"
-              class="alert-danger"
-            >{{errors.first('password')}}</div>
+              v-if="errors.has('password')"
+              class="alert alert-danger"
+              role="alert"
+            >Password do not match!</div>
+          </b-form-group>
+          <div class="login-button mt-2">
+            <b-button pill type="submit" variant="primary">SIGN UP</b-button>
           </div>
-          <div class="form-group text-center my-3">
-            <button type="submit" class="btn btn-success px-5">SIGN UP</button>
+          <div class="create-account mt-2">
+            <p>Already have an account?&nbsp;<a href="">Login here</a></p>
           </div>
-        </div>
-      </form>
-      <div
-        v-if="message"
-        class="alert"
-        :class="successTrue() ? 'alert-success' : 'alert-danger'"
-      >{{message}}</div>
-    </b-col>
-    <b-col></b-col>
-  </b-row>
-  <br />
-  <b-row>
-    <b-col></b-col>
-    <center>
-      <b-col>
-      </b-col>
-    </center>
-    <b-col></b-col>
-  </b-row>
-  <br />
-  <b-row class="text-center">
-    <b-col></b-col>
-    <b-col cols="8">
-      <p> Already have an account? Login here</p>
-    </b-col>
-    <b-col></b-col>
-  </b-row>
-</b-container>
+        </b-form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -170,45 +147,38 @@ export default {
 </script>
 
 <style scoped>
-.bv-example-row {
-  background-color: #ffffff;
-  border-radius: 5px;
-  box-shadow: 0 0px 2px 0
-            rgba(0, 0, 0, 0.3), 0
-            2px 10px 0 rgba(0, 0, 0, 0.5);
+.logo {
+  width: 250px;
+  margin:auto;
+}
+.login-google a {
+  width: 250px;
+  height: 45px;
+  background-color: #FFFFFF;
+  color: #000000;
+  font-size: 13px;
 }
 
-.btn-primary-Google {
-background: #ffffff;
-border-radius: 20px;
-}
-.background {
-  background-image: linear-gradient(to bottom right, #00DBDE, #3E006E);
-}
-
-.linea {
-  border-top: 1px solid black;
-  height: 2px;
-  padding: 0;
-  margin: 20px auto 0 auto;
-}
-
-.img-fluid{
-  width: 200px;
-  height: auto;
-}
-.emo {
-  width: 25px;
-  height: 25px;
-}
-
-.emo-face {
-  width: 25px;
-  height: 25px;
+.login-facebook img {
   filter: invert(100%) sepia(1%) saturate(7500%) hue-rotate(59deg) brightness(115%) contrast(100%);
 }
 
-.button-face{
-  background: #3b5998;
+.login-facebook a{
+  width: 250px;
+  height: 45px;
+  background-color: #3b5998;
+  font-size: 13px;
+}
+
+.line {
+  height: 2px;
+  background-color: #000000;
+  opacity: 0.2;
+}
+
+.login-form {
+  margin:auto;
+  width: 400px;
+  height: 35px;
 }
 </style>
