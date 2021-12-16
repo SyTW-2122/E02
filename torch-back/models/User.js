8978557c6136cdf55d8e028b2fa846b3b8d2a2f6
Eric DS <eric.d.s.info@gmail.com>
2021-12-16 17:33:14 +0000

    feat: add existing fix linter highlight

--- torch-back/models/User.js
+++ torch-back/models/User.js
@@ -16,30 +16,31 @@ const userSchema = new Schema({
 userSchema.pre('save', function (next) {
   var user = this;
   if (this.isModified('password') || this.isNew) {
-      bcrypt.genSalt(10, function (err, salt) {
-          if (err) {
-              return next(err);
-          }
-          bcrypt.hash(user.password, salt, null, function (err, hash) {
-              if (err) {
-                  return next(err);
-              }
-              user.password = hash;
-              next();
-          });
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, null, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
       });
-  } else {
-      return next();
+    });
+  }
+  else {
+    return next();
   }
 });
   
-userSchema.methods.comparePassword = function (passw, cb) {
-    bcrypt.compare(passw, this.password, function (err, isMatch) {
-        if (err) {
-            return cb(err);
-        }
-        return cb(null, isMatch);
-    });
+userSchema.methods.comparePassword = function bCryptCompare(passw, cb) {
+  bcrypt.compare(passw, this.password, (err, isMatch) => {
+    if (err) {
+      return cb(err);
+    }
+    return cb(null, isMatch);
+  });
 };
 
 module.exports = mongoose.model('User', userSchema);
