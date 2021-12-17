fb0d5964f08cad4e2a1c19e690e042b8f0956a53
Alejandro Martín de León <alu0101015941@ull.edu.es>
2021-12-17 13:33:37 +0000

    feat: saving before merge :gear:

--- torch-front/tests/unit/signIn/SignIn.spec.js
+++ torch-front/tests/unit/signIn/SignIn.spec.js
@@ -1,18 +1,94 @@
 import { defineFeature, loadFeature} from 'jest-cucumber';
 import { mount, createLocalVue} from '@vue/test-utils';
 import SignIn from '@/views/SignIn.vue';
+import App from '@/App.vue';
 
-const feature = loadFeature('./SignIn.feature');
-defineFeature(feature, (test) => {
-  test('Opening the sign in page', ({ given, when, then, and }) => {
+const feature = loadFeature('./SignIn.feature', { loadRelativePath: true, errors: true });
+defineFeature(feature, test => {
+  test('Opening the sign in page', ({
+    given,
+    when,
+    then,
+    and
+  }) => {
     given('the page is open in a browser', () => {
-     wrapper =  mount(SignIn);
+      const wrapper =  mount(App);
     });
+
     when('user inspects the page', () => {
-      // Nothing yet
+      
     });
-    then('user should see the User profile aspects', () => {
-      // Nothing yet
+
+    then('user should see the form login', () => {
+
+    });
+
+    and('page should contain logo', () => {
+
+    });
+
+    and('page should contain title', () => {
+
+    });
+
+    and('page should contain the option to remember the login information', () => {
+
+    });
+
+    and('page should contain the option to login with Google', () => {
+
+    });
+
+    and('page should contain the option to login with Facebook', () => {
+
+    });
+
+    and('When user taps on login buttom', () => {
+
+    });
+
+    then('page content is changed', () => {
+
+    });
+
+    and('the user view is displayed', () => {
+
+    });
+
+    and('When user taps on Create a torch account', () => {
+
+    });
+
+    then('page content is changed', () => {
+
+    });
+
+    and('the sign up view is displayed', () => {
+
+    });
+
+    and('When user taps on forgot password', () => {
+
+    });
+
+    then('page content is changed', () => {
+
+    });
+
+    and('When user taps on login with Google', () => {
+
+    });
+
+    then('page content is changed', () => {
+
+    });
+
+    and('When user taps on login with Facebook', () => {
+
+    });
+
+    then('page content is changed', () => {
+
     });
   });
 });
--- torch-front/tests/unit/signUp/SignUp.spec.js
+++ torch-front/tests/unit/signUp/SignUp.spec.js
@@ -3,91 +3,16 @@ import { mount, createLocalVue} from '@vue/test-utils';
 import SignUp from '@/views/SignUp.vue';
 
 const feature = loadFeature('./SignUp.feature');
-defineFeature(feature, test => {
-  test('Opening the sign in page', ({
-    given,
-    when,
-    then,
-    and
-  }) => {
+defineFeature(feature, (test) => {
+  test('Opening the sign up page', ({ given, when, then, and }) => {
     given('the page is open in a browser', () => {
-      wrapper =  mount(SignUp);
+     wrapper =  mount(SignUp);
     });
-
     when('user inspects the page', () => {
-
+      // Nothing yet
     });
-
-    then('user should see the form login', () => {
-
-    });
-
-    and('page should contain logo', () => {
-
-    });
-
-    and('page should contain title', () => {
-
-    });
-
-    and('page should contain the option to remember the login information', () => {
-
-    });
-
-    and('page should contain the option to login with Google', () => {
-
-    });
-
-    and('page should contain the option to login with Facebook', () => {
-
-    });
-
-    and('When user taps on login buttom', () => {
-
-    });
-
-    then('page content is changed', () => {
-
-    });
-
-    and('the activities view is displayed', () => {
-
-    });
-
-    and('When user taps on Create a torch account', () => {
-
-    });
-
-    then('page content is changed', () => {
-
-    });
-
-    and('the sign up view is displayed', () => {
-
-    });
-
-    and('When user taps on forgot password', () => {
-
-    });
-
-    then('page content is changed', () => {
-
-    });
-
-    and('When user taps on login with Google', () => {
-
-    });
-
-    then('page content is changed', () => {
-
-    });
-
-    and('When user taps on login with Facebook', () => {
-
-    });
-
-    then('page content is changed', () => {
-
+    then('user should see the User profile aspects', () => {
+      // Nothing yet
     });
   });
 });
