854a0d2229c544a586122cef6e2f7a8383d33552
Laura Cañizares Herrera <alu0101129945@ull.edu.es>
2021-12-17 13:43:24 +0000

    feat: update e2e test :gear:

--- torch-front/tests/e2e/specs/SignUp.feature
+++ torch-front/tests/e2e/specs/SignUp.feature
@@ -1,13 +1,6 @@
 Feature: User sign up
-  Scenario: User without an account created can not login and taps on register link
-    Given User is at the login main page
-    When User enters not valid username as "usu1" and password as "usu1"
-    And User clicks on button
-    Then User is not able to login
-    When User clicks on register link
-    Then User is at the register page
-
   Scenario: User register with username, email and password
-    When User enters username as "<username>", email as "<email>" and password as "<password>"
+    Given User is at the sign up page
+    When User enters username as '<username>', email as '<email>' and password as '<password>'
     And User clicks on sign up button
     Then User is fully registered and able to successfully login
--- torch-front/tests/e2e/specs/SignUp/index.js
+++ torch-front/tests/e2e/specs/SignUp/index.js
@@ -1,31 +1,8 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 const { visit } = cy;
 
-Given ('User is at the login main page', () => {
-    cy.visit('/', {timeout: 10000});
-    cy.viewport(375, 812);
-})
-
-When ('User enters not valid username as {string} and password as {string}', (username, password) => {
-    cy.get('#input-email').type(username)
-    cy.get('#input-password').type(password)
-})
-
-And ('User clicks on button', () => {
-    cy.get('.login-button').click()
-})
-
-// NOTE: username-alert must be visible here, but is not.
-Then ('User is not able to login', () => {
-    cy.get('.username-alert')
-})
-
-When ('User clicks on register link', () => {
-    cy.get('.forgot-password').click()
-})
-
-Then ('User is at the register page', () => {
-    cy.visit('/register', {timeout: 10000})
+Given ('User is at the sign up page', () => {
+    cy.visit('/sign-up', {timeout: 10000});
 })
 
 When ('User enters username as {string}, email as {string} and password as {string}', (username, email, password) => {
