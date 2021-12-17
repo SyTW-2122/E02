f52ac5767b8516a990eed19c345b6d16ae33aded
Laura Cañizares Herrera <alu0101129945@ull.edu.es>
2021-12-17 16:33:57 +0000

    feat: delete email get :gear:

--- torch-front/tests/e2e/specs/SignUp/index.js
+++ torch-front/tests/e2e/specs/SignUp/index.js
@@ -5,16 +5,15 @@ Given ('User is at the sign up page', () => {
     cy.visit('/sign-up');
 })
 
-When ('User enters username as {string}, email as {string} and password as {string}', (username,email, password) => {
+When ('User enters username as {string} and password as {string}', (username, password) => {
     cy.get('#input-username').type(username)
-    cy.get('#input-email').type(email)
     cy.get('#input-password').type(password)
 })
 
-And ('User clicks on sign up button', () => {
+And ('User clicks on sign up button and is fully registered', () => {
     cy.get('.signup-button').click()
 })
 
-Then ('User is fully registered and able to successfully login', () => {
-    cy.visit('/');
+Then ('User clicks on Login here and User go to the login page', () => {
+    cy.visit('/http://localhost:8080/');
 })
