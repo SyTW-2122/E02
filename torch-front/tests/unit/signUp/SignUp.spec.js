0a8ba35ae603873e9e5d3adb8b0c8c44c2c8a2d3
Laura Cañizares Herrera <alu0101129945@ull.edu.es>
2021-12-17 17:32:35 +0000

    feat: necessary to pull main :gear:

--- torch-front/tests/unit/signUp/SignUp.spec.js
+++ torch-front/tests/unit/signUp/SignUp.spec.js
@@ -1,18 +1,38 @@
-import { defineFeature, loadFeature} from 'jest-cucumber';
-import { mount, createLocalVue} from '@vue/test-utils';
-import SignUp from '@/views/SignUp.vue';
+import { shallowMount } from '@vue/test-utils'
+import SignUp from '@/views/SignUp.vue'
 
-const feature = loadFeature('./SignUp.feature');
-defineFeature(feature, (test) => {
-  test('Opening the sign up page', ({ given, when, then, and }) => {
-    given('the page is open in a browser', () => {
-     wrapper =  mount(SignUp);
-    });
-    when('user inspects the page', () => {
-      // Nothing yet
-    });
-    then('user should see the User profile aspects', () => {
-      // Nothing yet
-    });
-  });
-});
+describe('SignUp.vue', () => {
+  // Inspect the raw component options
+  it('has data', () => {
+    expect(typeof SignUp.data).toBe('function')
+  })
+})
+
+describe('SignUp.vue', () => {
+  const wrapper = shallowMount(SignUp)
+
+  test('does a wrapper exist', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('renders the correct markup', () => {
+    expect(wrapper.html()).toContain('Sign up page.')
+  })
+
+  test('should show the form element on the user register', () => {
+    expect(wrapper.find("form").exists()).toBe(true)
+  })
+
+  test('should contain input fields', () => {
+    expect(wrapper.find('form > input').exists()).toBe(true)
+  })
+
+  test('form should contain input fields with type="text"', () => {
+    expect(wrapper.get('input[type=tjavascriptext]').exists()).toBe(true)
+  })
+
+  test('button trigger event', async () => {
+    await wrapper.find('form > button').trigger('click')
+    expect(wrapper.emitted()).toHaveProperty('submit')
+  })
+})
