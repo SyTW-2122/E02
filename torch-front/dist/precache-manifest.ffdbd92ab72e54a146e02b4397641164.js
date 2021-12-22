118a824594769c95134b9836135b26b307dfdfe6
Eric DS <eric.d.s.info@gmail.com>
2021-12-22 21:29:23 +0000

    adding dist subtree

---
+++ torch-front/dist/precache-manifest.ffdbd92ab72e54a146e02b4397641164.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12bb96164d9a225407b8",
+    "url": "/SyTW-2122/E02/css/app.c3df5b26.css"
+  },
+  {
+    "revision": "75b4584c41d23aedfbb7",
+    "url": "/SyTW-2122/E02/css/chunk-vendors.8bda0bd5.css"
+  },
+  {
+    "revision": "ff52a82a4683f3dd219a3b85d36b0ff7",
+    "url": "/SyTW-2122/E02/img/Na_June_25.eps"
+  },
+  {
+    "revision": "8e11bb3e639dba53477ec70fc2d0a2a5",
+    "url": "/SyTW-2122/E02/img/Na_June_25.jpg"
+  },
+  {
+    "revision": "66a75bda2eeeda8d9acb3c67ab095804",
+    "url": "/SyTW-2122/E02/img/facebook-brands.66a75bda.svg"
+  },
+  {
+    "revision": "66a75bda2eeeda8d9acb3c67ab095804",
+    "url": "/SyTW-2122/E02/img/facebook.66a75bda.svg"
+  },
+  {
+    "revision": "ff629c02385f2f3a199b7e0b3065d077",
+    "url": "/SyTW-2122/E02/img/google.ff629c02.png"
+  },
+  {
+    "revision": "9d9a58da59291ec8e0a263148d01fb00",
+    "url": "/SyTW-2122/E02/img/torch-logo.9d9a58da.png"
+  },
+  {
+    "revision": "9d9a58da59291ec8e0a263148d01fb00",
+    "url": "/SyTW-2122/E02/img/torch-logo_Mesa de trabajo 1 copia.9d9a58da.png"
+  },
+  {
+    "revision": "ff597334024f053a8333671cd02ac75b",
+    "url": "/SyTW-2122/E02/index.html"
+  },
+  {
+    "revision": "12bb96164d9a225407b8",
+    "url": "/SyTW-2122/E02/js/app.16678b6f.js"
+  },
+  {
+    "revision": "75b4584c41d23aedfbb7",
+    "url": "/SyTW-2122/E02/js/chunk-vendors.46291fbd.js"
+  },
+  {
+    "revision": "00e44938f988941b9674e5fd9bb9a33f",
+    "url": "/SyTW-2122/E02/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/SyTW-2122/E02/robots.txt"
+  }
+]);
