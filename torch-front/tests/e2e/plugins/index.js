0e4aef5ce148d28cf213f30de7f95d284d5b18b1
Eric DS <eric.d.s.info@gmail.com>
2021-12-30 13:31:20 +0000

    config: add coverage tools and command for backend

--- torch-front/tests/e2e/plugins/index.js
+++ torch-front/tests/e2e/plugins/index.js
@@ -10,6 +10,7 @@
 // const webpack = require('@cypress/webpack-preprocessor')
 const cucumber = require('cypress-cucumber-preprocessor').default;
 module.exports = (on, config) => {
+  require('@cypress/code-coverage/task')(on, config);
   on("file:preprocessor", cucumber());
   
   return {
