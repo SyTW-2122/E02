aeb4f61cef475af90c924f22fab410d1e33cb0ec
Laura Cañizares Herrera <alu0101129945@ull.edu.es>
2022-01-07 19:38:27 +0000

    add class method to the explorer service :gear:

--- torch-front/src/services/explorer.service.js
+++ torch-front/src/services/explorer.service.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/explore/';
+const API_URL = 'http://localhost:5000/api/explore/';
 
 class ExplorerService {
-  getPublicContent = () => axios.get(`${API_URL}all`);
+  loadUserValoration = (explorer) => axios
+    .put(`${API_URL}`, {
+      userValoration: user.valoration,
+    });
+
+  loadRoutineValoration = (explorer) => axios
+    .put(`${API_URL}`, {
+      routineValoration: routine.valoration,
+    });
 }
 
 export default new ExplorerService();
