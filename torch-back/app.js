ed24ae3a0fec50882140ee5073ab223fa1315b54
Eric Dürr <eric.durr.20@ull.edu.com>
2022-01-10 18:13:13 +0000

    feat: removed bodyparser from backend, no size limit on JSON :gear:

--- torch-back/app.js
+++ torch-back/app.js
@@ -2,7 +2,6 @@ const express = require('express'),
   cors = require('cors'),
   path = require('path'),
   logger = require('morgan'),
-  bodyParser = require('body-parser'),
   userAPI = require('./route/user'),
   indexAPI = require('./route/staticIndex'),
   auth = require('./route/auth');
@@ -10,10 +9,8 @@ const express = require('express'),
 const app = express();
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false,
-}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
