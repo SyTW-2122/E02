adf5bd32e8db6a9858ee95750af58e3122be64e0
Eric Dürr <eric.durr.20@ull.edu.com>
2022-01-11 00:42:07 +0000

    app backend enhance :rocket:

--- torch-back/app.js
+++ torch-back/app.js
@@ -9,8 +9,8 @@ const express = require('express'),
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
