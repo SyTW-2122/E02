64259fa59026b71d72a6fd4f59daff9ad6e38f50
Eric Durr <eric.durr.20@ull.edu.es>
2022-01-12 23:30:49 +0000

    testing: modified User

--- torch-front/tests/e2e/specs/User/index.js
+++ torch-front/tests/e2e/specs/User/index.js
@@ -4,13 +4,13 @@ const { visit } = cy;
 Given ('I am logged in as "eric"', () => {
   cy.visit('/sign-in', {timeout: 10000});
   cy.viewport(375, 812);
-  cy.get('#input-email').type('eric');
+  cy.get('#input-email').type('Eric');
   cy.get('#input-password').type('123456');
   cy.get('.login-button').click();
 });
 
 When ('I look at the view', () => {
-  cy.url().should('include', '/user')
+  cy.url().should('include', '/Eric')
 });
 Then ('I should see all the profile layout as espected', () => {
   // User settings button row
