bec4836b95ef304711ef95c1cb87e30a1b8fba02
Laura Cañizares Herrera <alu0101129945@ull.edu.es>
2022-01-15 18:52:13 +0000

    feat: explorer route corrected :gear:

--- torch-back/route/explorer.js
+++ torch-back/route/explorer.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ExplorerController = require('../controllers/ExplorerController');
 
 const router = express.Router();
-router.get('/', ExplorerController.allExplorer);
+router.get('/', ExplorerController.allRoutines);
 router.get('/get/:username', ExplorerController.getUserByUsername);
 router.get('/get/:sportName', ExplorerController.getRoutineBySportName);
 
