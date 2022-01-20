2a089e409dcbcfd91049665b245e0114dca2d09d
Laura Cañizares Herrera <alu0101129945@ull.edu.es>
2022-01-20 20:30:45 +0000

    feat: add routineCard component :gear:

---
+++ torch-front/src/components/RoutineCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+    <b-row no-gutters>
+      <b-col md="6">
+        <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0"></b-card-img>
+      </b-col>
+      <b-col md="6">
+        <b-card-body title="Nombre deporte">
+          <b-card-text>
+            Descripción.
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'RoutineCard',
+  props: ['routine'],
+};
+</script>
+
+<style scoped>
+</style>
