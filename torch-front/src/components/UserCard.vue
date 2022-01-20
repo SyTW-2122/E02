45579b27baf2d9d655c54a2f87b3ce60d82b89da
Laura Cañizares Herrera <alu0101129945@ull.edu.es>
2022-01-20 20:31:05 +0000

    feat: add userCard component :gear:

---
+++ torch-front/src/components/UserCard.vue
@@ -0,0 +1,27 @@
+<template>
+   <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+    <b-row no-gutters>
+      <b-col md="6">
+        <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0"></b-card-img>
+      </b-col>
+      <b-col md="6">
+        <b-card-body title="Nombre usuario">
+          <b-card-text>
+            Valoración.
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  props: ['user'],
+};
+</script>
+
+<style scoped>
+
+</style>
