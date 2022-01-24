fa02cd8ac160af2b67c630af89b8d0613f48f87c
Laura Cañizares Herrera <alu0101129945@ull.edu.es>
2022-01-24 08:52:43 +0000

    feat: added users router :gear:

--- torch-back/route/explorer.js
+++ torch-back/route/explorer.js
@@ -3,6 +3,7 @@ const ExplorerController = require('../controllers/ExplorerController');
 
 const router = express.Router();
 router.get('/', ExplorerController.allRoutines);
+router.get('/', ExplorerController.allUsers);
 router.get('/get/:username', ExplorerController.getUserByUsername);
 router.get('/get/:sportName', ExplorerController.getRoutineBySportName);
 
