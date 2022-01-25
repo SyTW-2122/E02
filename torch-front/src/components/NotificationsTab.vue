4e98e8297ba8808bda49de3c32943dd0c049b80b
Eric DS <eric.d.s.info@gmail.com>
2022-01-25 21:00:08 +0000

    feat: fix tabs responsiveness

--- torch-front/src/components/NotificationsTab.vue
+++ torch-front/src/components/NotificationsTab.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+    <b-container v-if="user.newNotifications.length === 0">
+      <h2 class="text-secondary text-center border-bottom p-4">
+        no reccent activity for {{user.username}}
+      </h2>
+    </b-container>
     <b-row class="mt-2"
     v-for="(notification, index) in user.newNotifications" :key="index">
       <b-col>
         <b-card :bg-variant="bgColor(notification.viewed)" text-variant="white" >
           <b-card-text class="text-center">
-            <span class="me-5 pe-5">{{notification.text}}</span>
-            <span class="ms-5 ps-5">
+            <span class="text-center text-md-start me-md-5 pe-md-5">{{notification.text}}</span>
+            <span class="text-center text-md-end ms-md-5 ps-md-5">
               <b-button variant="primary" @click="view(notification.if)">
                 <font-awesome-icon icon="trash" class="fa-1x "/>
               </b-button>
--- torch-front/src/components/ProfileTools.vue
+++ torch-front/src/components/ProfileTools.vue
@@ -3,31 +3,36 @@
       <b-tabs content-class="mt-3" justified>
         <b-tab>
           <template #title>
-            <font-awesome-icon v-if="isPersonal" icon="history" class="fa-2x"/>
+            <font-awesome-icon
+            v-if="isPersonal"
+            icon="history"
+            class="fa-2x my-3"/>
             <font-awesome-icon v-else icon="stream" class="fa-2x"/>
           </template>
           <p>I'm the history tab</p>
         </b-tab>
         <b-tab>
           <template #title>
-            <font-awesome-icon v-if="isPersonal" icon="plus-square" class="fa-2x"/>
+            <font-awesome-icon
+            v-if="isPersonal" icon="plus-square"
+            class="fa-2x my-3"/>
             <font-awesome-icon v-else icon="newspaper" class="fa-2x"/>
           </template>
           <p>I'm the creator tab</p>
         </b-tab>
         <b-tab>
           <template #title>
-            <span
+            <div
               v-if="isPersonal && (user.newNotifications.length > 0)"
-              class="fa-stack fa-2x has-badge"
+              class="fa-stack fa-2x mt-1"
               :data-count="user.newNotifications.length">
-              <font-awesome-icon  icon="bell"/>
-            </span>
-            <span
+              <font-awesome-icon icon="bell"/>
+            </div>
+            <div
               v-else-if="isPersonal && (user.newNotifications.length === 0)"
-              class="fa-stack fa-2x has-badge">
+              class="fa-stack fa-2x">
               <font-awesome-icon  icon="bell"/>
-            </span>
+            </div>
             <font-awesome-icon v-else icon="star-half-alt" class="fa-2x"/>
           </template>
           <NotificationsTab v-if="isPersonal"
@@ -57,39 +62,17 @@ export default {
 </script>
 
 <style scoped>
-  .badge-notification{
-    width: 100px;
-      display: inline-block;
-    text-align: center;
-    vertical-align: middle;
-    position: relative;
-  }
-.badge-notification .badge:after {
-  content: "100";
-  position: absolute;
-  background: blue;
-  height: 2rem;
-  top: 1rem;
-  right: 1.5rem;
-  width: 2rem;
-  text-align: center;
-  line-height: 2rem;
-  font-size: 1rem;
-  border-radius: 50%;
-  color: white;
-  border: 1px solid blue;
-}
 .fa-stack[data-count]:after {
   position: absolute;
-  right: 0%;
-  top: 1%;
+  right: 25%;
+  top: 5%;
   content: attr(data-count);
-  font-size: 30%;
+  font-size: 33%;
   padding: .6em;
   border-radius: 999px;
   line-height: .75em;
   color: white;
-  background: rgba(255, 0, 0, 0.85);
+  background: rgba(255, 0, 0, 1);
   text-align: center;
   min-width: 2em;
   font-weight: bold;
