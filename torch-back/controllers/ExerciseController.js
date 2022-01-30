b085084655c37e8d607ec83ab6c30eca59e93eb9
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-01-30 20:03:51 +0000

    feat: get exercise from api now avaiable :gear:

--- torch-back/controllers/ExerciseController.js
+++ torch-back/controllers/ExerciseController.js
@@ -1,6 +1,20 @@
-const Routine = require('../models/Routine');
+//const Routine = require('../models/Routine');
 const Exercise = require('../models/Exercise');
 
 module.exports = {
-    
+  getExcercise: (req, res) => {
+    Exercise.findOne({
+      name: req.params.exercise,
+    }, (err, exer) => {
+      if (err) throw err;
+      if (!exer) {
+        res.status(401).send({ success: false, msg: 'Update failed. Exercise not found.' });
+      }
+      else {
+        res.status(200).json(exer);
+      }
+    })
+  },
+  addExcercise: (req, res) => {
+  }
 }
--- torch-back/route/exercise.js
+++ torch-back/route/exercise.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const UserController = require('../controllers/ExerciseController');
+const ExerciseController = require('../controllers/ExerciseController');
 
 const router = express.Router();
 
+router.get('/get/:exercise', ExerciseController.getExcercise);
+router.post('/createexercise/:username', ExerciseController.addExcercise);
 
 
 module.exports = router;
