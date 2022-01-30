6000376869470a57e1221b64ce6f5119d0f2bcdc
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-01-30 19:46:57 +0000

    feat: excercise files are now avaiable :gear:

---
+++ torch-back/controllers/ExerciseController.js
@@ -0,0 +1,6 @@
+const Routine = require('../models/Routine');
+const Exercise = require('../models/Exercise');
+
+module.exports = {
+    
+}
---
+++ torch-back/route/exercise.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const UserController = require('../controllers/ExerciseController');
+
+const router = express.Router();
+
+
+
+module.exports = router;
