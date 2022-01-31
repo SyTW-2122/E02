0ac6f57bbcca8c4b03239e1259f6a4d69f0ee104
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-01-31 18:28:37 +0000

    feat: first version of activity service file :gear:

--- torch-front/src/services/activity.service.js
+++ torch-front/src/services/activity.service.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-const API_URL = process.env.VUE_APP_USERAPIURL;
+const API_URL = process.env.VUE_APP_ACTIVITYAPIURL;
 class ActivityService {
-    
+  all = (username) => axios
+    .get(`${API_URL}/${username}`)
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    })
 }
+
+export default new ActivityService();
